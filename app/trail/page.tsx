3e0d63da977ef9ee9783cdd602f17b59035ac1ef
Kevin-Kaspar Einsok <114922098+Kassukas123@users.noreply.github.com>
2024-12-04 08:51:37 +0200

    feat: added database connection

---
+++ app/trail/page.tsx
@@ -0,0 +1,9 @@
+import TrailComponent from "../../components/trail/TrailComponent"; 
+
+export default function Trail() {
+    return (
+        <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0 }}>
+            <TrailComponent /> 
+        </div>
+    );
+}
---
+++ types/trail.ts
@@ -0,0 +1,15 @@
+export type Trail = {
+    id: number;
+    name: string;
+    type: string;
+    parking?: boolean;
+    camping?: boolean;
+    accessibility?: string;
+    location: string;
+    berries_mushrooms?: string;
+    sightseeing?: string;
+    xCoordinate: number;
+    yCoordinate: number;
+    picture?: string;
+  };
+  
