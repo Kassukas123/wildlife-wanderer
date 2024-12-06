d67159c25bf77d8ed0acddf32e90bc10e723daee
Kevin-Kaspar Einsok <114922098+Kassukas123@users.noreply.github.com>
2024-12-06 09:40:51 +0200

    feat: added remove filters button

--- components/map/FilterComponent.tsx
+++ components/map/FilterComponent.tsx
@@ -143,6 +143,24 @@ export function FilterComponent({
       >
         Rakenda filtreid
       </button>
+
+      <div className="mb-4">
+        <button
+          onClick={() =>
+            onFilterChange({
+              parking: null,
+              camping: null,
+              campfire: null,
+              length: null,
+              type: null,
+              accessibility: null,
+            })
+          }
+          className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+        >
+          Eemalda filtrid
+        </button>
+      </div>
     </div>
   );
 }
